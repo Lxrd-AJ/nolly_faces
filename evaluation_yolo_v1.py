5928e4b84a7164cbe6ffca36fa62d8a7c08426b9
AJ Ibraheem <ibraheemaj@outlook.com>
2019-09-23 05:21:52 +0100

    Yolo V1: Weights loading implemented

--- evaluation_yolo_v1.py
+++ evaluation_yolo_v1.py
@@ -9,10 +9,12 @@ from models.yolo_v1 import Yolo_V1
 if __name__ == "__main__":
     imagenet_config = "./models/yolov1.cfg"
     model = Yolo_V1(imagenet_config)
+    model.load_weights("./models/yolov1.weights")
     model.eval()
-    x = torch.randn(5,3,448,448)
+    x = torch.randn(2,3,448,448)
     # x = x.unsqueeze(0)
     res = model(x)    
     print(res.size())
     res = torch.flatten(res).view(x.size()[0],-1)
     print(res.size())
+    print(res.numel())
--- models/yolo_v1.py
+++ models/yolo_v1.py
@@ -31,6 +31,86 @@ class Yolo_V1(nn.Module):
         det_tensor = lin_out.view(-1,((self.num_bound_boxes*5) + self.num_classes),self.grid,self.grid)
         return det_tensor #torch.flatten(det_tensor)
 
+    def load_weights(self, weights_file):
+        with open(weights_file,'rb') as file:
+            #NB: An internal file pointer is maintained, so read the header first
+            header = np.fromfile(file, dtype=np.int32, count=5)
+            weights = np.fromfile(file, dtype=np.float32)
+            idx = 0  
+            # populate the convolutional layers           
+            for i in range(len(self.conv_layers)):
+                block_info = self.blocks[i+1]
+                if block_info['type'] in ['convolutional','local']:                    
+                    try:
+                        batch_norm = bool(block_info['batch_normalize'])
+                    except:
+                        batch_norm = False
+                    conv = self.conv_layers[i][0]
+                    if batch_norm: #load the biases, weights, running_mean and running variance
+                        bn = self.conv_layers[i][1]
+                        num_bn_bias = bn.bias.numel()
+                        bn_bias = torch.from_numpy(weights[idx:idx+num_bn_bias])
+                        idx += num_bn_bias
+                        # batch norm weights
+                        num_bn_weights = bn.weight.numel()
+                        assert(num_bn_bias == num_bn_weights)
+                        bn_weights = weights[idx:idx+num_bn_weights]
+                        idx += num_bn_bias
+                        bn_weights = torch.from_numpy(bn_weights)
+                        # batch norm running mean
+                        assert( num_bn_bias == bn.running_mean.numel())
+                        bn_running_mean = torch.from_numpy(weights[idx:idx+num_bn_bias])
+                        idx += num_bn_bias
+                        # running variance
+                        bn_running_variance = torch.from_numpy(weights[idx:idx+num_bn_bias])
+                        idx += num_bn_bias
+
+                        #convert the loaded params into the same size as the target bias params
+                        bn_bias = bn_bias.view_as(bn.bias.data)
+                        bn_weights = bn_weights.view_as(bn.weight.data)
+                        bn_running_mean = bn_running_mean.view_as(bn.running_mean)
+                        bn_running_variance = bn_running_variance.view_as(bn.running_var)
+                        # copy the loaded params into the bias params
+                        bn.bias.data.copy_(bn_bias)
+                        bn.weight.data.copy_(bn_weights)
+                        bn.running_mean.copy_(bn_running_mean)
+                        bn.running_var.copy_(bn_running_variance)
+                    else: #copy the conv bias
+                        num_conv_bias = conv.bias.numel()
+                        conv_bias = torch.from_numpy(weights[idx:idx+num_conv_bias])
+                        idx += num_conv_bias
+                        conv_bias = conv_bias.view_as(conv.bias.data)
+                        conv.bias.data.copy_(conv_bias)
+                    # copy the conv weights
+                    num_conv_weights = conv.weight.numel()
+                    conv_weights = torch.from_numpy(weights[idx:idx+num_conv_weights])
+                    idx += num_conv_weights
+                    conv_weights = conv_weights.view_as(conv.weight.data)
+                    conv.weight.data.copy_(conv_weights)
+            
+            # Populate the linear layers
+            for k in range(len(self.linear_layers)):
+                print(self.linear_layers[k])
+                linear = self.linear_layers[k]
+                # copy the bias
+                num_lin_bias = linear.bias.numel()
+                lin_bias = torch.from_numpy(weights[idx:idx+num_lin_bias])
+                lin_bias = lin_bias.view_as(linear.bias.data)
+                linear.bias.data.copy_(lin_bias)
+                idx += num_lin_bias
+                # copy the weights
+                num_lin_weights = linear.weight.numel()
+                lin_weights = torch.from_numpy(weights[idx:idx+num_lin_weights])
+                lin_weights = lin_weights.view_as(linear.weight.data)
+                linear.weight.data.copy_(lin_weights)
+                idx += num_lin_weights
+
+            print("Index into weights", idx)
+            print("Total weights ", len(weights))
+            print("Num weights left ", len(weights) - idx)
+            print("---" * 15)
+                        
+
     def conv(self, item, prev_filters, filters, batch_norm): 
         padding = int(item['pad'])
         kernel_size = int(item['size'])
